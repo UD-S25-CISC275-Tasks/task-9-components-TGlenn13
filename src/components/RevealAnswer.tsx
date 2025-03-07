cefb06b5274163b81f3da6a6cf2c18e35c4895d2
Thomas Glenn <tglenn@udel.edu>
2025-03-06 19:17:29 -0500

    Reveal answer solved

--- src/components/RevealAnswer.tsx
+++ src/components/RevealAnswer.tsx
@@ -2,5 +2,18 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function RevealAnswer(): React.JSX.Element {
-    return <div>Reveal Answer</div>;
+    const [revealed, setRevealed] = useState<boolean>(false);
+    const [answer] = useState<number>(42);
+    return (
+        <span>
+            <Button
+                onClick={() => {
+                    setRevealed(!revealed);
+                }}
+            >
+                Reveal Answer
+            </Button>
+            {revealed && <span> {answer} </span>}
+        </span>
+    );
 }
